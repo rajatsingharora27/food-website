da3d930a60ce3f2e3a86a8732fdddb140587cd45
rajat <rajatsingharora27@gmail.com>
2023-02-13 13:22:30 +0530

    added redux store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import NavBar from "./Components/NavBar/NavBar";
 import { Outlet } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./Redux/ReduxStore/Store";
+import Accordian from "./Components/Accordian/Accordian";
 
 const App = () => {
   return (
     <>
-      <NavBar />
-      <Outlet />
+      <Provider store={store}>
+        <NavBar />
+        <Accordian />
+        <Outlet />
+      </Provider>
     </>
   );
 };
