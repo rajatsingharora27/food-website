2be400443741a568184f56327f2bffe3f6ab5746
rajat <rajatsingharora27@gmail.com>
2023-02-19 19:10:14 +0530

    css chages

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { FaSearch, FaUserAlt, FaListUl, FaCartPlus } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import "../../root.css";
+
 // import SearchComponent from "../SearchComponent";
 
 const NavBar = () => {
@@ -17,7 +19,7 @@ const NavBar = () => {
 
   return (
     <>
-      <div className="w-auto shadow-md h-[60px] flex justify-between items-center text-white bg-[#acdccb] z-10 md:w-screen">
+      <div className="w-auto shadow-md h-[60px] flex justify-between items-center text-white bg-[var(--background-color)] z-10 md:w-screen">
         <Link to={"/home"}>
           <img
             src="/images/logo.png"
@@ -28,41 +30,41 @@ const NavBar = () => {
 
         <ul className="hidden lg:flex px-4  ">
           <Link to={"/home"}>
-            <li className="text-xl px-4 cursor-pointer hover:text-[#c7b299] hover:ease-in duration-100">
+            <li className="text-xl px-4 cursor-pointer hover:text-[var(--navBarHover-color)] hover:ease-in duration-100">
               Home
             </li>
           </Link>
           <Link to={"/about"}>
-            <li className="text-xl px-4 cursor-pointer hover:text-[#c7b299] hover:ease-in duration-100 ">
+            <li className="text-xl px-4 cursor-pointer hover:text-[var(--navBarHover-color)] hover:ease-in duration-100 ">
               About Us
             </li>
           </Link>
 
           <Link to={"/cakes"}>
-            <li className="text-xl px-4  cursor-pointer hover:text-[#c7b299] hover:ease-in duration-100">
+            <li className="text-xl px-4  cursor-pointer hover:text-[var(--navBarHover-color)] hover:ease-in duration-100">
               Cakes
             </li>
           </Link>
           <Link to={"/gift-hamper"}>
-            <li className="text-xl px-4 cursor-pointer hover:text-[#c7b299] hover:ease-in duration-100 ">
+            <li className="text-xl px-4 cursor-pointer hover:text-[var(--navBarHover-color)] hover:ease-in duration-100 ">
               Gift Hampers
             </li>
           </Link>
 
           <Link to={"/savory"}>
-            <li className="text-xl px-4 cursor-pointer hover:text-[#c7b299] hover:ease-in duration-100 ">
+            <li className="text-xl px-4 cursor-pointer hover:text-[var(--navBarHover-color)] hover:ease-in duration-100 ">
               Savory
             </li>
           </Link>
 
           <Link to={"/pastory"}>
-            <li className="text-xl px-4 cursor-pointer hover:text-[#c7b299] hover:ease-in duration-100 ">
+            <li className="text-xl px-4 cursor-pointer hover:text-[var(--navBarHover-color)] hover:ease-in duration-100 ">
               Pastory
             </li>
           </Link>
 
           <Link to={"/contact"}>
-            <li className="text-xl px-4 cursor-pointer hover:text-[#c7b299] hover:ease-in duration-100">
+            <li className="text-xl px-4 cursor-pointer hover:text-[var(--navBarHover-color)] hover:ease-in duration-100">
               Contact Us
             </li>
           </Link>
@@ -71,9 +73,13 @@ const NavBar = () => {
           <FaSearch className="mx-4 cursor-pointer" />
           <FaUserAlt className="mx-4 cursor-pointer" />
           <FaCartPlus className="mx-4 cursor-pointer z-[1]" />
-          <span className="absolute top-2 right-0 py-0 px-1 text-[#c7b299] rounded-full text-[13px] align-top ml-[-10px] bg-[#ff0000c1] ">
-            {cartItems.length}
-          </span>
+          {cartItems.length > 0 ? (
+            <span className="absolute top-2 right-0 py-0 px-1 text-[var(--navBarHover-color)] rounded-full text-[13px] align-top ml-[-10px] bg-[#ff0000c1] ">
+              {cartItems.length}
+            </span>
+          ) : (
+            ""
+          )}
         </div>
         {/* HamBurger */}
         <div className="lg:hidden z-10">
@@ -89,48 +95,48 @@ const NavBar = () => {
           onClick={handleNav}
           className={
             nav
-              ? "overflow-y-hidden lg:hidden ease-in duration-300 w-full absolute left-0 top-0 h-screen bg-[#acdccb]/90 px-4 py-7 flex flex-col"
+              ? "overflow-y-hidden lg:hidden ease-in duration-300 w-full absolute left-0 top-0 h-screen bg-[var(--background-color)] px-4 py-7 flex flex-col"
               : "absolute top-0 h-screen left-[-100%] ease-in duration-500"
           }
         >
           <div>
             <ul className="h-full w-full text-center pt-12">
               <Link to={"/home"}>
-                <li className="text-xl px-5  py-4 cursor-pointer hover:text-[#c7b299] hover:ease-in duration-100">
+                <li className="text-xl px-5  py-4 cursor-pointer hover:text-[var(--navBarHover-color)] hover:ease-in duration-100">
                   Home
                 </li>
               </Link>
               <Link to={"/about"}>
-                <li className="text-xl px-5  py-4 cursor-pointer hover:text-[#c7b299] hover:ease-in duration-100 ">
+                <li className="text-xl px-5  py-4 cursor-pointer hover:text-[var(--navBarHover-color)] hover:ease-in duration-100 ">
                   About Us
                 </li>
               </Link>
 
               <Link to={"/cakes"}>
-                <li className="text-xl px-5 py-4 cursor-pointer hover:text-[#c7b299] hover:ease-in duration-100">
+                <li className="text-xl px-5 py-4 cursor-pointer hover:text-[var(--navBarHover-color)] hover:ease-in duration-100">
                   Cakes
                 </li>
               </Link>
               <Link to={"/gift-hamper"}>
-                <li className="text-xl px-5 py-4  cursor-pointer hover:text-[#c7b299] hover:ease-in duration-100 ">
+                <li className="text-xl px-5 py-4  cursor-pointer hover:text-[var(--navBarHover-color)] hover:ease-in duration-100 ">
                   Gift Hampers
                 </li>
               </Link>
 
               <Link to={"/savory"}>
-                <li className="text-xl px-5 py-4 cursor-pointer hover:text-[#c7b299] hover:ease-in duration-100 ">
+                <li className="text-xl px-5 py-4 cursor-pointer hover:text-[var(--navBarHover-color)] hover:ease-in duration-100 ">
                   Savory
                 </li>
               </Link>
 
               <Link to={"/pastory"}>
-                <li className="text-xl px-5  py-4 cursor-pointer hover:text-[#c7b299] hover:ease-in duration-100 ">
+                <li className="text-xl px-5  py-4 cursor-pointer hover:text-[var(--navBarHover-color)] hover:ease-in duration-100 ">
                   Pastory
                 </li>
               </Link>
 
               <Link to={"/contact"}>
-                <li className="text-xl px-5 py-4 cursor-pointer hover:text-[#c7b299] hover:ease-in duration-100">
+                <li className="text-xl px-5 py-4 cursor-pointer hover:text-[var(--navBarHover-color)] hover:ease-in duration-100">
                   Contact Us
                 </li>
               </Link>
