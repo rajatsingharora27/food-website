3787e0ede6e61eed2e3a4fb5b27d977b9e31e371
rajat <rajatsingharora27@gmail.com>
2023-05-07 18:24:55 +0530

    added few changes in cakes page

--- src/Pages/Cakes/Cakes.js
+++ src/Pages/Cakes/Cakes.js
@@ -11,29 +11,35 @@ function Cakes() {
           alt=""
           className="h-auto max-w-full"
         />
-        <div className="absolute left-1/3 top-1/3 md:left-1/2 md:top-1/2">
+        <div className="absolute left-1/3 top-1/3 md:left-1/2">
           <h1 className="font-sans text-4xl">Cakes</h1>
         </div>
       </div>
       <div>FILTER BAR</div>
-      <div className="max-w-xs p-6 rounded-md shadow-md dark:bg-gray-900 dark:text-gray-50">
-        <img
-          src="https://source.unsplash.com/random/300x300/?1"
-          alt=""
-          className="object-cover object-center w-full rounded-md h-72 dark:bg-gray-500"
-        />
-        <div className="mt-6 mb-2">
-          <span className="block text-xs font-medium tracking-widest uppercase dark:text-violet-400">
-            Quisque
-          </span>
-          <h2 className="text-xl font-semibold tracking-wide">
-            Nam maximus purus
-          </h2>
-        </div>
-        <p className="dark:text-gray-100">
-          Mauris et lorem at elit tristique dignissim et ullamcorper elit. In
-          sed feugiat mi. Etiam ut lacinia dui.
-        </p>
+      <div className="flex flex-wrap justify-center items-center gap-y-3 gap-x-3">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(() => {
+          return (
+            <div className="max-w-xs p-6 rounded-md shadow-md dark:bg-gray-900 dark:text-gray-50">
+              <img
+                src="https://source.unsplash.com/random/300x300/?1"
+                alt=""
+                className="object-cover object-center w-full rounded-md h-72 dark:bg-gray-500"
+              />
+              <div className="mt-6 mb-2">
+                <span className="block text-xs font-medium tracking-widest uppercase dark:text-violet-400">
+                  Quisque
+                </span>
+                <h2 className="text-xl font-semibold tracking-wide">
+                  Nam maximus purus
+                </h2>
+              </div>
+              <p className="dark:text-gray-100">
+                Mauris et lorem at elit tristique dignissim et ullamcorper elit.
+                In sed feugiat mi. Etiam ut lacinia dui.
+              </p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
