da3f9daf81b1060c9c4a7bfbdbfd70cb4c2c179d
rajat <rajatsingharora27@gmail.com>
2023-07-15 23:37:21 +0530

    added new components

--- src/Pages/Cakes/Cakes.js
+++ src/Pages/Cakes/Cakes.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PageCard from "../../Components/PageCard/PageCard";
 
 function Cakes() {
   return (
@@ -9,36 +10,16 @@ function Cakes() {
         <img
           src="https://cdn.shopify.com/s/files/1/0580/2668/8535/files/banner.jpg?v=1679214987"
           alt=""
-          className="h-auto max-w-full"
+          className="h-auto max-w-full opacity-60"
         />
         <div className="absolute left-1/3 top-1/3 md:left-1/2">
-          <h1 className="font-sans text-4xl">Cakes</h1>
+          <h1 className="font-sans text-4xl text-[#333333] ">Cakes</h1>
         </div>
       </div>
       <div>FILTER BAR</div>
       <div className="flex flex-wrap justify-center items-center gap-y-3 gap-x-3">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(() => {
-          return (
-            <div className="max-w-xs p-6 rounded-md shadow-md dark:bg-gray-900 dark:text-gray-50">
-              <img
-                src="https://source.unsplash.com/random/300x300/?1"
-                alt=""
-                className="object-cover object-center w-full rounded-md h-72 dark:bg-gray-500"
-              />
-              <div className="mt-6 mb-2">
-                <span className="block text-xs font-medium tracking-widest uppercase dark:text-violet-400">
-                  Quisque
-                </span>
-                <h2 className="text-xl font-semibold tracking-wide">
-                  Nam maximus purus
-                </h2>
-              </div>
-              <p className="dark:text-gray-100">
-                Mauris et lorem at elit tristique dignissim et ullamcorper elit.
-                In sed feugiat mi. Etiam ut lacinia dui.
-              </p>
-            </div>
-          );
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((cardDetail) => {
+          return <PageCard />;
         })}
       </div>
     </div>
